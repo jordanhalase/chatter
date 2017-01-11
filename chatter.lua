e19ebd597aebf6a63e7c04b7f12e4078b09323c3
Owen Monsma <darkfire613@me.com>
2017-01-10 17:50:00 -0800

    recursive xml traversal works

--- chatter.lua
+++ chatter.lua
@@ -1,11 +1,14 @@
 local chatter = {}
 
 function chatter:loadFile(filename)
-  local pattern = "<(%a+)%s(%a+)='(%d+)'>"
-  local lineCount = 1
+  io.input(filename)
+  local file = io.read("*all")
+  -- removes all new lines and indents
+  file = string.gsub(file, "\n%s*", "")
+  --print(file)
+  self:_parseFile(file)
 
-  local chat
-
+  --[[
   for line in io.lines(filename) do
     _, _, tag, attName, attVal = string.find(line, pattern)
     if tag == "chat" then
@@ -18,6 +21,7 @@ function chatter:loadFile(filename)
     end
     lineCount = lineCount + 1
   end
+  ]]
 end
 
 function chatter:print(chatID)
@@ -25,8 +29,39 @@ function chatter:print(chatID)
 
 end
 
+function chatter:_printAll()
+  for _, conv in pairs(self.conv) do
+    for _, chat in pairs(conv) do
+      for  _, item in pairs(chat) do
+        print(item)
+      end
+    end
+  end
+end
+
 function chatter:_parseFile(str)
-  local pattern = "<.*>.*<.*>"
+  local pattern = "^<(.-)%s?(.-)>(.-)</%1>(.*)"
+  -- splits string into current semantic block, and the remainder
+  local i, j, tag, params, contents, remainder = string.find(str, pattern)
+  -- if
+  if tag then
+    print(tag)
+    chatter:_parseFile(contents)
+  end
+
+
+  if remainder then
+    --print(remainder)
+    chatter:_parseFile(remainder)
+  end
+end
+
+function chatter:_parseTagParams(params)
+  local pattern = ""
+  local _, _, par, val = string.find(params, pattern)
+  local loadedParams = {}
+  loadedParams[par] = val
+  return loadedParams
 end
 
 return chatter
